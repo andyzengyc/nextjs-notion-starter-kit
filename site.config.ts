70379e707c0bd546c8f2029052121a230fdd7ac5
andyzengyc <150079000+andyzengyc@users.noreply.github.com>
2023-11-06 15:37:33 -0500

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,20 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '3d10bd6edce6402e9588b915e7e4ce8f',
+
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Andy Zeng',
+  domain: 'alluring-viscose-c04.notion.site',
+  author: 'Andy Zeng',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Andy Zeng',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
